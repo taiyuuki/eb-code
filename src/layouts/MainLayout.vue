40a82c6593644a24c4356973093d2d70ba7f92f7
taiyuuki <taiyuuki@qq.com>
2024-06-16 11:40:37 +0800

    feat: set the font-size of the Monaco Editor

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,10 +3,43 @@ import TitleBar from '@/components/TitleBar.vue'
 import { useCompRef } from '@/composables/useComp'
 import { contents_setting } from '@/composables/contents_setting'
 import { cover_setting } from '@/composables/cover_setting'
+import { set_font_size } from '@/editor'
+import { useFontSize } from '@/stores/font-size'
+import { DISPLAY } from '@/static'
+import { useStatus } from '@/stores/status'
 
 const titlebar = useCompRef(TitleBar)
+const font_size = useFontSize()
+const status = useStatus()
+
 onBeforeMount(() => {
     document.documentElement.classList.add('monaco-component')
+
+    set_font_size(font_size.size)
+
+    document.addEventListener('keydown', function(e: KeyboardEvent) {
+        if (
+            e.key === 'F5'
+            || e.ctrlKey && e.key === 'r'
+            || e.metaKey && e.key === 'r'
+        ) {
+            e.preventDefault()
+        } else if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
+            e.preventDefault()
+            titlebar.value.save_epub()
+        } else if (status.display === DISPLAY.CODE) {
+            if (e.key === '-' || e.key === '_' && e.ctrlKey) {
+                e.preventDefault()
+                font_size.size = Math.max(font_size.size - 2, 8)
+                set_font_size(font_size.size)
+            } else if (e.key === '+' || e.key === '=' && e.ctrlKey) {
+                e.preventDefault()
+                font_size.size = Math.min(font_size.size + 2, 40)
+                set_font_size(font_size.size)
+            }
+        }
+    })
+
     if (window.location.hostname !== 'tauri.localhost') {
         return
     }
@@ -22,21 +55,6 @@ onBeforeMount(() => {
 
         return false
     }, { capture: true })
-
-    document.addEventListener('keydown', function(e: KeyboardEvent) {
-        if (
-            e.key === 'F5'
-            || e.ctrlKey && e.key === 'r'
-            || e.metaKey && e.key === 'r'
-        ) {
-            e.preventDefault()
-        }
-
-        if (e.key === 's' && (e.ctrlKey || e.metaKey)) {
-            e.preventDefault()
-            titlebar.value.save_epub()
-        }
-    })
 })
 
 function open() {

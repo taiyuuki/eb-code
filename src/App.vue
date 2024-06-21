429257bb0cbe22757ed14dcd2f8040f1f5854b00
taiyuuki <taiyuuki@qq.com>
2024-06-21 11:21:37 +0800

    fix: clean cache before reload the page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,22 @@
 <script setup lang="ts">
+import { invoke_clean_cache } from './invoke'
 import { useStatus } from './stores/status'
-import { invoke_setup } from './invoke'
-import { notif_negative } from './notif'
-import { basename } from './utils/path'
 
 const status = useStatus()
+
 defineOptions({ name: 'App' })
 
-const observer = new PerformanceObserver(list => {
-    list.getEntries().forEach(entry => {
-        const navi_type = Reflect.get(entry, 'type')
-        if (navi_type === 'navigate') {
-            invoke_setup().then(payload => {
-                status.current.save_path = payload.save_path
-                status.set_dir(payload.dir)
-                status.set_base_path(payload.base_path)
-                status.parse_epub(payload)
-            }, e => {
-                if (e !== '0') {
-                    notif_negative(`${basename(e)}不是有效的EPUB文件。`)
-                }
-            }) 
-        } 
-    })
+window.addEventListener('beforeunload', e => {
+    const msg = '\\o/'
+    e.preventDefault()
+    e.returnValue = msg
+
+    return msg
 })
 
-observer.observe({ type: 'navigation', buffered: true })
+window.addEventListener('unload', () => {
+    status.dir && invoke_clean_cache(status.dir)
+})
 </script>
 
 <template>

7ccf39845da4b6fa3bbff1fecd6e2a4f58651e8b
taiyuuki <taiyuuki@qq.com>
2024-06-30 18:58:38 +0800

    fix: persist of the option

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -2,13 +2,15 @@ import beautify from 'js-beautify'
 import { useOption } from '@/stores/option'
 import stores from '@/stores'
 
-const option = useOption(stores)
-
 function fmt_html(html: string) {
+    const option = useOption(stores)
+
     return beautify.html(html, { indent_size: option.value.indent })
 }
 
 function fmt_css(css: string) {
+    const option = useOption(stores)
+
     return beautify.css(css, { indent_size: option.value.indent })
 }
 
